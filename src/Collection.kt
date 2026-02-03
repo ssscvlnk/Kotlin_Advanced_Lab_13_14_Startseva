22243ae19bebb7d8819059eff2c5b8dc737040bd
ssscvlnk <pstartceva@yandex.ru>
2026-02-03 22:04:34 +0300

    Lists in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -15,15 +15,34 @@ val newSolarSystem = arrayOf(
     "Pluto"
 )
 fun main() {
-    println(solarSystem[0])
-    println(solarSystem[1])
+//    println(solarSystem[0])
+//    println(solarSystem[1])
+//    println(solarSystem[2])
+//    println(solarSystem[3])
+//    println(solarSystem[4])
+//    println(solarSystem[5])
+//    println(solarSystem[6])
+//    println(solarSystem[7])
+//    solarSystem[3] = "Little Earth"
+//    println(solarSystem[3])
+//    println(newSolarSystem[8])
+    val solarSystem = mutableListOf(
+        "Mercury", "Venus", "Earth", "Mars",
+        "Jupiter", "Saturn", "Uranus", "Neptune"
+    )
+    solarSystem.add("Pluto")
+    solarSystem.add(3,"Theia")
+    println(solarSystem.size)
     println(solarSystem[2])
+    println(solarSystem.get(2))
+    println(solarSystem.indexOf("Earth"))
+    println(solarSystem.indexOf("Pluto"))
+    for (planet in solarSystem) {
+        println(planet)
+    }
+    solarSystem[3] = "Future Moon"
     println(solarSystem[3])
-    println(solarSystem[4])
-    println(solarSystem[5])
-    println(solarSystem[6])
-    println(solarSystem[7])
-    solarSystem[3] = "Little Earth"
-    println(solarSystem[3])
-    println(newSolarSystem[8])
+    println(solarSystem[9])
+    solarSystem.removeAt(9)
+    solarSystem.remove("Future Moon")
 }
