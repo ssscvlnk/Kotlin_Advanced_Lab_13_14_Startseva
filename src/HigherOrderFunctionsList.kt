dea3bdc4c540835cc55143043ce632d3acc71c78
ssscvlnk <pstartceva@yandex.ru>
2026-02-04 14:10:47 +0300

    sortedBy() function

--- src/HigherOrderFunctionsList.kt
+++ src/HigherOrderFunctionsList.kt
@@ -81,4 +81,9 @@ fun main() {
         total + cookie.price
     }
     println("Total price: $${totalPrice}")
+    val alphabeticalMenu = cookies.sortedBy { it.name }
+    println("Меню в алфавитном порядке:")
+    alphabeticalMenu.forEach {
+        println(it.name)
+    }
 }
